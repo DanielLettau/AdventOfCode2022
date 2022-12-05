c5a4b7dc031c66c8639cca2c305d8db3943afdf6
Daniel Lettau <daniel.lettau@ing.de>
2022-12-05 16:04:10 +0100

    day3 correction

--- day3/day3_1.ps1
+++ day3/day3_1.ps1
@@ -1,9 +1,11 @@
 $data = gc .\input.txt
-$prio = [char[]](0..255) -like '[A-Z]'
+$prio = @{}
+65..90  | % {$prio.add([char]$_,$_-38)}
+97..122 | % {$prio.add([char]$_,$_-96)}
 $totalprio = foreach ($d in $data) {
     $compartment1 = $d.Substring(0,$d.Length/2).ToCharArray()
     $compartment2 = $d.Substring($d.Length/2).ToCharArray()
     $item = (Compare-Object -ExcludeDifferent -IncludeEqual $compartment1 $compartment2).inputobject | Select-Object -Unique
-    $prio.indexof([string]$item)+1
+    $prio.$item
 }
 $totalprio | measure -sum | select -exp sum
